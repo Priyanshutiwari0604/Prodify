2384eedbb6c94b99b8fc915cc1d8d8159f184bee
Priyanshutiwari0604 <kvstudent245@gmail.com>
2025-07-25 04:03:20 +0530

    front end commit message

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -7,10 +7,7 @@ const router = express.Router();
 
 
 router.get("/",getProducts);
-// Test route
-router.get("/", (req, res) => {
-    res.json({ message: "Server is running!" });
-});
+
 
 router.post("/",createProducts);
 
